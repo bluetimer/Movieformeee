656e1273f502e7c5ffff865200d053ea98ddb2d2
Ethan Yehuda <eyehuda@salesforce.com>
2020-05-12 17:06:39 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
+/* global $ */
+
+$("#search-button").click(function(){
+  
+  
+  
+});
+
