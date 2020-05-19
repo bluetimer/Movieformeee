f761a90bfa08caeae16f0295ba431cd1109acd81
Issac Teoh <teohissac@gmail.com>
2020-05-19 22:06:26 +0000

    added hello world

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
+//hello world!
 $("#search-button").click(function(){
   
   
